03e27cfec7e12b34fa2e7e466f2ca26ab093dbdf
Edward Feng <edward0414@gmail.com>
2018-06-24 15:03:15 -0400

    Finished the hackathon

--- AssetTracker.sol
+++ AssetTracker.sol
@@ -1,49 +1,42 @@
 pragma solidity ^0.4.18;
 // We have to specify what version of compiler this code will compile with
 
-contract AssetTracker {
+contract recordTracker {
   /* mapping field below is equivalent to an associative array or hash.
-  The key of the mapping is Asset name stored as type bytes32 and value is
-  an unsigned integer to store the vote count
   */
-  event AssetAdded(bytes32 asset, bool assetCount);
+    event RecordAdded(bytes32 Record, bool submitted);
 
-  mapping (bytes32 => bool) public assetInventory;
+    mapping (bytes32 => bool) public recordDocument;
   
-  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
-  We will use an array of bytes32 instead to store the list of Assets
-  */
-  
-  bytes32[] public AssetList;
+    bytes32[] public RecordList;
 
   /* This is the constructor which will be called once when you
   deploy the contract to the blockchain. When we deploy the contract,
-  we will pass an array of Assets who will be contesting in the election
+  we will pass an array of Records who will be used
   */
-  function AssetTracker(bytes32[] AssetNames) public {
-    AssetList = AssetNames;
-  }
-
-  // This function returns the total votes a Asset has received so far
-  function totalByAsset(bytes32 Asset) view public returns (bool) {
-    require(validAsset(Asset));
-    return assetInventory[Asset];
-  }
-
-  // This function increments the vote count for the specified Asset. This
-  // is equivalent to casting a vote
-  function addAssetToInventory(bytes32 Asset) public {
-    require(validAsset(Asset));
-    assetInventory[Asset] = true;
-    emit AssetAdded(Asset, assetInventory[Asset]);
-  }
-
-  function validAsset(bytes32 Asset) view public returns (bool) {
-    for(uint i = 0; i < AssetList.length; i++) {
-      if (AssetList[i] == Asset) {
-        return true;
-      }
+    function recordTracker(bytes32[] RecordNames) public {
+        RecordList = RecordNames;
     }
-    return false;
-  }
+
+  // This function returns the record
+    function showRecord(bytes32 Record) view public returns (bool) {
+        require(validRecord(Record));
+        return recordDocument[Record];
+    }
+
+  // This function adds the record to the document map
+    function addRecordToDocument(bytes32 Record) public {
+        require(validRecord(Record));
+        recordDocument[Record] = true;
+        emit RecordAdded(Record, recordDocument[Record]);
+    }
+
+    function validRecord(bytes32 Record) view public returns (bool) {
+        for(uint i = 0; i < RecordList.length; i++) {
+            if (RecordList[i] == Record) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
