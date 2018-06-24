d620bff8e07c4834176471b148572c1d5c372450
Angela Lee <angelalee.one@gmail.com>
2018-06-24 12:03:14 -0400

    generic alert

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"AssetList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"assetInventory","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"Asset","type":"bytes32"}],"name":"totalByAsset","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"Asset","type":"bytes32"}],"name":"addAssetToInventory","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"Asset","type":"bytes32"}],"name":"validAsset","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"AssetNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"asset","type":"bytes32"},{"indexed":false,"name":"assetCount","type":"uint8"}],"name":"AssetAdded","type":"event"}]');
 AssetTrackerContract = web3.eth.contract(abi);
 // In your nodejs AssetTrackerContract, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = AssetTrackerContract.at('0x1ff8fbc91d1226abeaa507663db99cf80270dffa');
+contractInstance = AssetTrackerContract.at('0x0f2edf0e0c9bb84fb2b4260ce5b222299a8d1b16');
 assets = {"Waivers": "asset-1", "Conditions": "asset-2", "Legal": "asset-3", "Money": "asset-4"}
+assetsArray = ['Waivers', 'Conditions', 'Legal', 'Money'];
 isCompleteArray = [false, false, false, false];
 //deployedContract = AssetTrackerContract.new(['Waivers', 'Conditions', 'Legal', 'Money'], {data: byteCode, from:web3.eth.accounts[0], gas:4700000})
 // ['Waivers', 'Conditions', 'Legal', 'Money']
@@ -52,7 +53,7 @@ function checkAllDone() {
       return false;
     }
   } 
-  var doneElem = document.getElementById("done")
+  var doneElem = document.getElementById("done");
   if (doneElem) {
     doneElem.style.visibility="visible"; 
   } 
@@ -62,15 +63,18 @@ var assetEvent = contractInstance.AssetAdded();
 
 assetEvent.watch(function(error, result){
   if (!error){
-      console.log("no error");
-      alert("Event result:" + result);
+      console.log(result);
+      var index = result.args.assetCount.c[0] + 1;
+      var assetType = assetsArray[index];
+
+      alert("An update was made to your home purchase, please check your account");
   } else {
       console.log(error);
   }
 })
 
 $(document).ready(function() {
-  var doneElem = document.getElementById("done")
+  var doneElem = document.getElementById("done");
   if (doneElem) {
     doneElem.style.visibility="hidden"; 
   } 
